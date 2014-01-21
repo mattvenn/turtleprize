0eb8d85725fa90079b9d39ae3f40abd012713e35
Matt Venn <matt@mattvenn.net>
2014-01-21 08:53:23 +0000

    loop in a loop demo

---
+++ 2loop_demo.py
@@ -0,0 +1,8 @@
+loop_1=0
+while loop_1 < 3:
+    loop_2 = 0
+    while loop_2 < 3:
+        print("loop_1=",loop_1)
+        print("loop_2=",loop_2)
+        loop_2 = loop_2 + 1
+    loop_1 = loop_1 + 1
